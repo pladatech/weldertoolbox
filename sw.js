95988fedf86e520e299d94f0bf3c7a632542023a
pladatech <arielwelding@gmail.com>
2026-06-28 23:44:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = 'pladatech-tools-v2';
-const ASSETS = [
+const CACHE_NAME = 'toolbox-cache-v1';
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon.png',
-  'https://cdn.tailwindcss.com'
+  './icon.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
   );
   self.skipWaiting();
 });
@@ -33,8 +33,9 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
